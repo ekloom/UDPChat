f20ca33a797fecb1aa40a9fcceebf4840e619676
elidev14 <ek.asiamah@student.avans.nl>
2026-06-13 15:54:26 +0200

    small changes

--- UDPChatClient/Program.cs
+++ UDPChatClient/Program.cs
@@ -6,25 +6,49 @@
         public static async Task Main(string[] args)
         {
 
+            string? hostName = null;
+            int portNumber;
+
             if (args.Length != 2)
             {
-                Console.Error.WriteLine("Usage: <host name> <port number>");
-                Environment.Exit(1);
-            }
+                Console.Write("Enter the hostname: ");
+                hostName = Console.ReadLine();
 
-            string hostName = args[0];
-            if (!int.TryParse(args[1], out int portNumber))
+                Console.Write("Enter the port number: ");
+                if (!int.TryParse(Console.ReadLine(), out portNumber))
+                {
+                    Console.Error.WriteLine("Port number moet een getal zijn.");
+                    Console.ReadLine();
+                    Environment.Exit(1);
+                }
+            }
+            else
             {
-                Console.Error.WriteLine("Port number moet een getal zijn.");
-                Environment.Exit(1);
+
+                hostName = args[0];
+                if (!int.TryParse(args[1], out portNumber))
+                {
+                    Console.Error.WriteLine("Usage: <host name> <port number>");
+                    Console.Error.WriteLine("Port number moet een getal zijn.");
+                    Console.ReadLine();
+                    Environment.Exit(1);
+                }
             }
 
+            if (string.IsNullOrEmpty(hostName))
+            {
+                Console.WriteLine("No host name was provided");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
 
             Console.WriteLine("Welkom bij UDPChat!");
 
 
             try
             {
+
+                // TODO, First check if hostname + port exist
 
                 ClientHandler clientHandler = new ClientHandler(hostName, portNumber);
                 await clientHandler.ConnectAsync();
